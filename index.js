bacac75777a2582120df6cc464961ee3d401c70a
Karan-Bharti1 <karan.codes1@gmail.com>
2025-04-01 10:21:01 +0530

    feat: added api for user to sign up

--- index.js
+++ index.js
@@ -11,4 +11,27 @@ const corsOptions={
 }
 app.use(cors(corsOptions))
 initialiseDatabase()
-
+const User=require("./models/Owner.models")
+const ownerSignUp = async (data) => {
+    try {
+       
+        const newUser=new User(data)
+        const savedData = await newUser.save();
+        return savedData;
+    } catch (error) {
+        throw error;
+    }
+};
+app.post("/signup",async(req,res)=>{
+try {
+    const data=await ownerSignUp(req.body)
+    if(data){
+        res.status(200).json({message:"Account Created Successfully",data})
+    }
+} catch (error) {
+    res.status(500).json({message:"Failed To add user to database"})
+}
+})
+app.listen(PORT,()=>{
+  console.log( `App is running at ${PORT}`)
+})
