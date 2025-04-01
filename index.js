8b6f7934da76a08053aef0e9fadc19af9130ec21
Karan-Bharti1 <karan.codes1@gmail.com>
2025-04-01 11:10:26 +0530

    added api to login user ,creating tokens on login ,adding protected route and verifying token on connection to protected route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {initialiseDatabase}=require("./databse/database.connection")
 const cors=require("cors")
 const PORT=3000
+const jwt=require("jsonwebtoken")
 const express=require('express')
 const app=express()
 app.use(express.json())
@@ -11,6 +12,23 @@ const corsOptions={
 }
 app.use(cors(corsOptions))
 initialiseDatabase()
+//Middleware to verify token
+const verifyJWT=(req,res,next)=>{
+    const token=req.headers['authorization']
+    if(!token){
+        res.status(401).json({message:"No token was found"})
+    }
+    try {
+
+        const decodedToken=jwt.verify(token,JWT_SECRET)
+        req.user=decodedToken;
+        next()
+        } catch (error) {
+        res.status(402).json({message:"Invalid Token"})
+        
+    }
+}
+const JWT_SECRET="your_jwt_secret"
 const User=require("./models/Owner.models")
 const ownerSignUp = async (data) => {
     try {
@@ -31,6 +49,20 @@ try {
 } catch (error) {
     res.status(500).json({message:"Failed To add user to database"})
 }
+})
+app.post("/login",async(req,res)=>{
+    const user=req.body
+    const Secret_Key=await User.findOne(user)
+    if(Secret_Key.password===user.password && Secret_Key.email===user.email){
+const token=jwt.sign({role:"admin"},JWT_SECRET,{expiresIn:"24h"})
+res.json({token})
+    }else{
+res.json({message:"Invalid Secret"})
+    }
+
+})
+app.get("/task/api/data",verifyJWT,async(req,res)=>{
+res.json({message:"Protected route accessible"})
 })
 app.listen(PORT,()=>{
   console.log( `App is running at ${PORT}`)
