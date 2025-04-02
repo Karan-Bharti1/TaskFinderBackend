8df531bb556265f93bc95a93a823452dfae368a6
Karan-Bharti1 <karan.codes1@gmail.com>
2025-04-02 19:44:38 +0530

    refactor: updated api to check existing email exists with condition

--- index.js
+++ index.js
@@ -62,7 +62,8 @@ app.get("/users/existing",async(req,res)=>{
     
     try {
         const existingUser = await User.findOne({ email });
-        res.json({ message:"user already exists",existingUser });
+       if(existingUser){
+        res.json({ message:"user already exists",existingUser })};
     } catch (error) {
         res.status(500).json({ message: "Server error" });
     }
