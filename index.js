1635f361a36d8c5c37e212691bde7f09471a1269
Karan-Bharti1 <karan.codes1@gmail.com>
2025-04-05 07:56:03 +0530

    refactor:updated cors options

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express=require('express')
 const app=express()
 app.use(express.json())
 const corsOptions={
-        origin:'http://localhost:5173/',
+        origin:'*',
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true,
         optionSuccessStatus:200
